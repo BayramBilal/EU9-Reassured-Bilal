ac533d05b93cb6986879426314b73fe2dca30353
Bilal <105069025+BayramBilal@users.noreply.github.com>
2022-11-19 16:59:11 +0300

    Pojo, json to pojo and lombok annotations practices are added.

--- src/test/java/com/cydeo/utilities/SpartanTestBase.java
+++ src/test/java/com/cydeo/utilities/SpartanTestBase.java
@@ -1,9 +1,13 @@
 package com.cydeo.utilities;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
+import java.util.Map;
+
 import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
 
 public class SpartanTestBase {
 
@@ -12,17 +16,23 @@ public class SpartanTestBase {
         //save baseurl inside this variable so that we dont need to type each http method.
         baseURI = "http://54.88.101.116:8000";
 
-        //get ip address from configuraitons
+        //get ip address from configurations
         String dbUrl = "jdbc:oracle:thin:@54.88.101.116:1521:xe";
+      //  String dbUsername = "SP";
+        //  String dbPassword = "SP";
+
+      //  DBUtils.createConnection(dbUrl, dbUsername, dbPassword);
+
+
         String dbUsername = "hr";
         String dbPassword = "hr";
 
-        //  DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
+         // DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
     }
 
     @AfterAll
     public static void teardown(){
 
-        //DBUtils.destroy();
+       // DBUtils.destroy();
     }
 }
